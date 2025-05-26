f0fd950c706180d98df34b21e20eb5ef57011314
Jang Tae Gyeom <127314485+yeon-hong@users.noreply.github.com>
2025-05-26 21:31:52 +0900

    [level 1] Title: 폰켓몬, Time: 0.74 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,12 +1,4 @@
 def solution(nums):
-    answer = 0
-    arr = []
-
-    for i in nums:
-        if i not in arr:
-            arr.append(i)
-            answer += 1
-        if len(nums)/2 == len(arr):
-            break
-        
+    answer = min(len(nums)/2, len(set(nums)))
+    
     return answer
