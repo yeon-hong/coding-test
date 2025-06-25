27617b53fb8f150f8e965d02cb9ccf9912df0fab
Jang Tae Gyeom <127314485+yeon-hong@users.noreply.github.com>
2025-06-25 22:52:47 +0900

    [level 1] Title: 옹알이 (2), Time: 0.23 ms, Memory: 9.14 MB -BaekjoonHub

---
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
@@ -0,0 +1,24 @@
+def solution(babbling):
+    answer = 0
+    for word in babbling:
+        if can_pronounce(word):
+            answer += 1
+    return answer
+
+def can_pronounce(word):
+    sounds = ["aya", "ye", "woo", "ma"]
+    prev = ''
+    while word:
+        matched = False
+        for sound in sounds:
+            if word.startswith(sound) and sound != prev:
+                word = word[len(sound):]
+                prev = sound
+                matched = True
+                break
+        if not matched:
+            return False
+    return True
+
+            
+        
