ebc41ab823455f1deba5678878efad0775b4fab0
Jang Tae Gyeom <127314485+yeon-hong@users.noreply.github.com>
2025-07-12 23:40:43 +0900

    [level 2] Title: 가장 큰 수, Time: 61.49 ms, Memory: 26.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -0,0 +1,6 @@
+def solution(numbers):
+    strs = list(map(str, numbers))
+
+    strs.sort(key=lambda x: x*3, reverse=True)
+    answer = ''.join(strs)
+    return '0' if answer[0] == '0' else answer
