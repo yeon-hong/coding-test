160126cdf03fa229ca284b352036870d1f6c1621
Jang Tae Gyeom <127314485+yeon-hong@users.noreply.github.com>
2025-07-16 00:23:48 +0900

    [level 2] Title: 시소 짝꿍, Time: 7.83 ms, Memory: 13.1 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.py
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.py
@@ -1,13 +1,14 @@
 from collections import Counter
+
 def solution(weights):
     answer = 0
-    counter = Counter(weights)
+    counter = Counter(weights)  # 무게별 개수 세기
 
-    for i in range(100, 1001): # 1:1, 2:3, 2:4, 3:4 인 경우의 수.
-        if counter[i] > 0:
-            answer += (counter[i] * (counter[i]-1))/2 # 1:1 조합 nC2 = n(n-1)/2
-            answer += counter[i] * counter[i * 3 / 2] # 2:3
-            answer += counter[i] * counter[i * 2] # 2:4
-            answer += counter[i] * counter[i * 4 / 3] # 3:4
+    for i in range(100, 1001):  # 몸무게 범위: 100 ~ 1000
+        if counter[i] > 0:  # 해당 무게가 존재할 때만 계산
+            answer += (counter[i] * (counter[i] - 1)) / 2  # 같은 무게끼리 1:1 거리 짝 계산 (조합 nC2)
+            answer += counter[i] * counter[i * 3 / 2]       # 2:3 거리 비율에 맞는 짝 수 계산
+            answer += counter[i] * counter[i * 2]           # 2:4 거리 비율에 맞는 짝 수 계산
+            answer += counter[i] * counter[i * 4 / 3]       # 3:4 거리 비율에 맞는 짝 수 계산
 
     return answer
