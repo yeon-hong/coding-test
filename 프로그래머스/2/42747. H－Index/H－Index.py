5de7bdd1feffa7a68b17b511e969254ed43a09d4
Jang Tae Gyeom <127314485+yeon-hong@users.noreply.github.com>
2025-08-18 15:27:28 +0900

    [level 2] Title: H-Index, Time: 0.11 ms, Memory: 9.23 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -0,0 +1,11 @@
+def solution(citations):
+    answer = 0
+    citations.sort()
+    n = len(citations)
+
+    for idx, c in enumerate(citations):
+        if n - idx <= c:
+            answer = n - idx
+            break
+    
+    return answer
