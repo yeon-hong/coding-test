26f69fd0e0181e99801107df47d9bac534ec1ce1
Jang Tae Gyeom <127314485+yeon-hong@users.noreply.github.com>
2025-08-19 22:18:54 +0900

    [level 2] Title: 2 x n 타일링, Time: 38.54 ms, Memory: 9.11 MB -BaekjoonHub

---
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.py
@@ -0,0 +1,10 @@
+def solution(n):
+    if n == 1:
+        return 1
+    if n == 2:
+        return 2
+    
+    a, b = 1, 2
+    for _ in range(3, n + 1):
+        a, b = b, a + b  
+    return b % 1000000007
