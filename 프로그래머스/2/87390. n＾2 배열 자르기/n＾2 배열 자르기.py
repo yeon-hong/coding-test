1e18e97b299e82bc310a798e76225200d59c0642
Jang Tae Gyeom <127314485+yeon-hong@users.noreply.github.com>
2025-08-20 16:23:25 +0900

    [level 2] Title: n^2 배열 자르기, Time: 50.27 ms, Memory: 18 MB -BaekjoonHub

---
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.py
@@ -0,0 +1,7 @@
+def solution(n, left, right):
+    answer = []
+    for k in range(left, right + 1):
+        row = k // n
+        col = k % n
+        answer.append(max(row, col) + 1)
+    return answer
